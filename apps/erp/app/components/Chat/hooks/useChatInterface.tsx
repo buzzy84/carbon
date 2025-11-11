54c8ac9d4ecb17b81450c728eeeee34c46426e5e
Brad Barbin <bradbarbin@protonmail.com>
2025-11-10 19:43:30 -0500

    refactor: url param for chat

--- apps/erp/app/components/Chat/hooks/useChatInterface.tsx
+++ apps/erp/app/components/Chat/hooks/useChatInterface.tsx
@@ -1,27 +1,26 @@
 import { useMount } from "@carbon/react";
-import { useUrlParams } from "@carbon/remix";
-import { useLocation } from "@remix-run/react";
+import { useOptimisticLocation, useUrlParams } from "@carbon/remix";
 import { generateId } from "ai";
 import { useCallback } from "react";
 import { path } from "~/utils/path";
 
 export function useChatInterface() {
   const [params, setParams] = useUrlParams();
-  const location = useLocation();
-  const chatId = params.get("chatId") || null;
+  const location = useOptimisticLocation();
+  const chatId = params.get("c") || null;
 
   const isChatPage = !!chatId;
   const isHome = location.pathname === path.to.authenticatedRoot;
 
   useMount(() => {
     if (isHome && !chatId) {
-      setParams({ chatId: generateId() });
+      setParams({ c: generateId() });
     }
   });
 
   const setChatId = useCallback(
     (id: string) => {
-      setParams({ chatId: id });
+      setParams({ c: id });
     },
     [setParams]
   );
